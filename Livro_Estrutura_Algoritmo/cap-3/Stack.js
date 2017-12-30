6ef1d118ff1924f855d247edf61b9de5c6685d77
Tiago Nunes <tiagonunes.developer@gmail.com>
2017-12-30 20:36:31 -0300

    :checkered_flag: Adicionando os comentarios nas class Stack e Queue :+1:

--- Livro_Estrutura_Algoritmo/cap-3/Stack.js
+++ Livro_Estrutura_Algoritmo/cap-3/Stack.js
@@ -8,35 +8,43 @@ const Stack = (function () {
       items.set(this, []);
     }
 
+    //adiciona um novo item (ou vários itens) ao topo da pilha
     push(element) {
       let s = items.get(this);
       s.push(element);
     }
 
+    //remove o time do topo da pilha e devolve o elemento removido
     pop() {
       let s = items.get(this);
       return s.pop();
     }
 
+    //devolve o elemento que está no topo da pilha
     peek() {
       let s = items.get(this);
       return s[s.length - 1];
     }
 
+    //devolve true se a pilha não contiver nenhum elemento e
+    //false se o tamanho da pilha for maior que 0
     isEmpty() {
       let s = items.get(this);
       return s.length == 0;
     }
 
+    //devolve o número de elementos contido na pilha
     size() {
       let s = items.get(this);
       return s.length;
     }
 
+    //remove todos os elementos da pilha
     clear() {
       return items.set(this, []);
     }
 
+    //exibe os conteúdo da pilha
     print() {
       let s = items.get(this);
       return s.toString();
---
+++ Livro_Estrutura_Algoritmo/cap-4/Queue.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const Queue = (function () {
+
+  const items = new WeakMap();
+
+  class Queue {
+    constructor() {
+      items.set(this, []);
+    }
+
+    //adiciona um novo item (ou vários) ao final da fila
+    enqueue(elemento) {
+      return items.get().push(elemento);
+    }
+
+    //remove o primeiro item da fila e também devolve o elemento removido
+    dequeue() {
+      return items.get().shift();
+    }
+
+    //devolve o primeiro elemento da fila
+    front() {
+      return items.get()[0];
+    }
+
+    //devolve true se a fila não contiver nenhum elemento 
+    //e false se ela for maior que 0
+    isEmpty() {
+      return items.get().lenght === 0;
+    }
+
+    //devolve o número de elementos contidos na fila
+    size() {
+      return items.get().lenght;
+    }
+  }
+
+})();
